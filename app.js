ff9dc7e748e45f14fa7b1294cbf822dacb0de187
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-01-26 19:52:09 -0500

    kdog this is a comment. You figured out how to use nunjucks

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const express = require( 'express' );
+const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express(); // creates an instance of an express application
 
 
+const people = [
+    { name: 'Gandalf' },
+    { name: 'Frodo' },
+    { name: 'Hermione' }
+];
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    watch: true,
+    noCache: true
+});
 // THIS IS THE SERVER
 
-app.use('/special', function(req, res, next){
+app.use('/special', function(req, res, next) {
     console.log('Youve reached the speacial area.');
     next();
 });
 
-app.use(function(req, res, next){
+/*app.use(function(req, res, next) {
     // get all
     // queues up the request by building an array or a queue or something
     // console.log(req.method, req.url);
@@ -26,33 +39,19 @@ app.use(function(req, res, next){
     console.log(req.method + '/' + res.statusCode);
     next();
 });
+*/
 
-
-app.get('/', function(req, res, next){
-   res.send('hello'); // res creates a new instance of the response object
-
+app.get('/', function(req, res, next) {
+    let nunjuckMagic = nunjucks.render('index.html', { people, title: 'An Example' });
+    res.send(nunjuckMagic); // res creates a new instance of the response object
 });
 
-app.get('/news', function(req, res, next){
+app.get('/news', function(req, res, next) {
     res.send('big news');
     req.url = '123';
-    console.log(req.url);
 });
 
-
-
-app.use(function(error, req, res, next){
-    console.log(error);
-    console.log(res);
-    console.log('here')
-});
-
-
-
-
-app.listen(3000, function(){
+app.listen(3000, function() {
     console.log('server is running')
 
 });
-
-
